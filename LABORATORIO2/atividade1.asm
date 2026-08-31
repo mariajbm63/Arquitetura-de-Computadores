cbce1edd3f07bbf9dd7a39654861bb79d8112b16
Maria J <mjbm@cin.ufpe.br>
2026-08-31 15:56:15 -0300

    Update atividade1.asm

--- LABORATORIO2/atividade1.asm
+++ LABORATORIO2/atividade1.asm
@@ -1,1 +1,15 @@
+lw x10, a
+lw x11, b
+add x12, x0, x10 # m recebe a
+sw x12, m
+blt x11, x12, SOMA
+jal x0, FIM
+SOMA: 
+add x12, x10, x11
+sw x12, m
+FIM:
+halt
 
+a: .word 0x0014
+b: .word 0x0007
+m: .word 0x0000
