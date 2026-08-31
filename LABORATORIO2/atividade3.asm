b919a0c30e50479717af9dce6349fc99943f2119
Maria J <mjbm@cin.ufpe.br>
2026-08-31 15:57:16 -0300

    Implement conditional addition and subtraction
    Added conditional logic to perform addition or subtraction based on comparison.

--- LABORATORIO2/atividade3.asm
+++ LABORATORIO2/atividade3.asm
@@ -1,1 +1,8 @@
-
+#Considerando que f, g, h, i, j  estão armazenados respectivamente 
+em x19, x20, x21, x22 e x23:
+bne x22, x23, Else 
+add x19, x20, x21
+beq x0, x0, Exit
+Else:
+sub x19, x20, x21
+Exit:
