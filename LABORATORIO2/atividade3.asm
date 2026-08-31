68cd01fa8b58ae903fa0e0011c5f426570fb2c07
Maria J <mjbm@cin.ufpe.br>
2026-08-31 15:57:29 -0300

    Fix comment formatting in atividade3.asm

--- LABORATORIO2/atividade3.asm
+++ LABORATORIO2/atividade3.asm
@@ -1,5 +1,4 @@
-#Considerando que f, g, h, i, j  estão armazenados respectivamente 
-em x19, x20, x21, x22 e x23:
+#Considerando que f, g, h, i, j  estão armazenados respectivamente em x19, x20, x21, x22 e x23:
 bne x22, x23, Else 
 add x19, x20, x21
 beq x0, x0, Exit
