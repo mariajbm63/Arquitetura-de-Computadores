c01da4f25485b14d6e6f8e92ac314320e783795f
Maria J <mjbm@cin.ufpe.br>
2026-08-31 15:56:48 -0300

    Implement addition and subtraction in atividade2.asm

--- LABORATORIO2/atividade2.asm
+++ LABORATORIO2/atividade2.asm
@@ -1,1 +1,15 @@
+lw x10, a
+lw x11, b
+addi x12, x0, 0 # m = 0
+blt x11, x12, SOMA
+sub x12, x10, x11
+jal x0, SALVA
+SOMA: 
+add x12, x10, x11
+SALVA:
+sw x12, m
+halt
 
+a: .word 6
+b: .word 15
+m: .word 0
